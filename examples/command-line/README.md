a58d82fded0a9911c54d6939965fbdeb9235a3aa
Eric Miller <github@patternleaf.com>
2019-05-06 16:29:04 -0600

    Command line run utility.

---
+++ examples/command-line/README.md
@@ -0,0 +1,8 @@
+Example running from command line. Can be useful for headless testing of graphs.
+
+From the flow-calc root directory, 
+
+```
+node ./src/run.js -g examples/command-line/graph-definition.json -i examples/command-line/inputs.json
+```
+
--- src/run.js
+++ src/run.js
@@ -1,4 +1,39 @@
 const DGraph = require('./index')
-const args = require('yargs').argv
+const yargs = require('yargs')
 
-// TODO: ability to take a graph and inputs (files or command line) and run the graph
+const args = yargs.option('graph-definition', {
+	alias: 'g',
+	describe: 'Path to json file describing your graph. A JSON array of nodes.'
+}).option('inputs', {
+	alias: 'i',
+	describe: 'Path to json file providing inputs to run against the graph definition.'
+}).demandOption(
+	['graph-definition', 'inputs'], 
+	'Please provide both graph-definition and inputs.'
+).help().argv
+
+function tryToLoad(path) {
+	let result
+	const prefixes = [__dirname, process.cwd(), '']
+	for (let prefix of prefixes) {
+		if (!result) {
+			try {
+				result = require(`${prefix}/${path}`)
+			}
+			catch (e) { }
+		}
+	}
+	if (!result) {
+		throw new Error(`Could not find ${path}.`)
+	}
+	return result
+}
+
+const graphDef = tryToLoad(args['graph-definition'])
+const inputs = tryToLoad(args['inputs'])
+
+
+const g = new DGraph(graphDef)
+g.run(inputs).then(results => {
+	console.log(JSON.stringify(results, null, 4))
+})
